8a6ac02333c013993c9a3e15c259bf0ff52a2e12
tu_nombre <luciapazmanagement@gmail.com>
2025-06-20 11:02:06 -0300

    Guardo mis cambios

--- TPI-SistemaLogistica-Equipo3B/Transportistas.aspx.cs
+++ TPI-SistemaLogistica-Equipo3B/Transportistas.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Gestion;
 
 namespace TPI_SistemaLogistica_Equipo3B
 {
@@ -17,6 +19,22 @@ namespace TPI_SistemaLogistica_Equipo3B
         protected void btnAñadirTransportista_Click(object sender, EventArgs e)
         {
             Response.Redirect("AgregarTransportista.aspx");
+        }
+
+        protected void btnTodos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnActivo_Click(object sender, EventArgs e)
+        {
+            GestionTransportista gestionTransportista = new GestionTransportista();
+            
+        }
+
+        protected void btnInactivo_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
